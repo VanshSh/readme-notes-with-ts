54f0f2080b702dcccc1be1e3c00992b9ed13c19c
VanshSh <vanshsharma9354@gmail.com>
2024-06-19 22:38:46 +0530

    Dummy routed created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {
+  Navigate,
   Outlet,
   Route,
   RouterProvider,
@@ -8,17 +9,23 @@ import {
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route
-      path='/'
-      element={
-        <div>
-          <h1>Hello VNAHkjkS</h1>
-          <Outlet />
-        </div>
-      }
-    >
-      <Route path='show' element={<h1>Hello SHOW</h1>} />
-    </Route>
+    <>
+      <Route
+        path='/'
+        element={
+          <div>
+            <h1>Hello VNAHkjkS</h1>
+            <Outlet />
+          </div>
+        }
+      />
+      <Route path='/new' element={<h1>Hello NEW PAGE</h1>} />
+      <Route path='/:id'>
+        <Route index element={<h1>Helo SHOW</h1>} />
+        <Route path='edit' element={<h1>Helo EDIT</h1>} />
+      </Route>
+      <Route path='*' element={<Navigate to='/' />} />
+    </>
   )
 )
 const App = () => {
